c2e914bba6a3b2c4f391593b78c3f8cf2f85e48a
REN <reny58817@gmail.com>
2024-11-29 12:28:24 +0900

    揺れと効果音

--- SleepingGames/Assets/Script/yure.cs
+++ SleepingGames/Assets/Script/yure.cs
@@ -1,41 +1,13 @@
-using System.Collections;
 using UnityEngine;
 
 public class yure : MonoBehaviour
 {
+    public yurecamera cameraShake;
     public float shakeDuration = 0.5f;
     public float shakeMagnitude = 0.1f;
-    private Vector3 initialPosition;
 
-    void Start()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        initialPosition = transform.localPosition;
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            StartCoroutine(Shake());
-        }
-    }
-
-    IEnumerator Shake()
-    {
-        float elapsed = 0.0f;
-
-        while (elapsed < shakeDuration)
-        {
-            float x = Random.Range(-1f, 1f) * shakeMagnitude;
-            float y = Random.Range(-1f, 1f) * shakeMagnitude;
-
-            transform.localPosition = new Vector3(x, y, initialPosition.z);
-
-            elapsed += Time.deltaTime;
-
-            yield return null;
-        }
-
-        transform.localPosition = initialPosition;
+        StartCoroutine(cameraShake.Shake(shakeDuration, shakeMagnitude));
     }
 }
